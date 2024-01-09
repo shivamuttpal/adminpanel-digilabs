ece3414da8d7c403671f7e94b2e332bb2c7cf5e3
shivam uttpal jha <shivamuttpal@gmail.com>
2024-01-09 10:31:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'
 
-function App() {
+const YourComponent = () => {
+  const [formData, setFormData] = useState({
+    logoUrl: '',
+    buttonText: '',
+    userEmail: '',
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.put('https://digilab-backend-qz1q.onrender.com/api/settings', formData);
+
+      // Handle the response as needed
+      alert("data updated");
+      console.log('Settings updated successfully:', response.data);
+    } catch (error) {
+      // Handle errors
+      console.error('Error updating settings:', error.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleFormSubmit}>
+      <label>
+        Logo URL:
+        <input type="text" name="logoUrl" value={formData.logoUrl} onChange={handleInputChange} />
+      </label>
+      <label>
+        Button Text:
+        <input type="text" name="buttonText" value={formData.buttonText} onChange={handleInputChange} />
+      </label>
+      <label>
+        User Email:
+        <input type="text" name="userEmail" value={formData.userEmail} onChange={handleInputChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
 
-export default App;
+export default YourComponent;
